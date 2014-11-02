3e5cdbd66c33f9f72ca492c9f2090acacb29a988
Dogbert <rockhowse@gmail.com>
2014-11-01 22:06:42 -0500

    Added official application.

--- Planetoid/Planetoid.cpp
+++ Planetoid/Planetoid.cpp
@@ -5,8 +5,9 @@
 #include "BulletOpenGLApplication.h"
 #include "FreeGLUTCallbacks.h"
 
-int _tmain(int argc, _TCHAR* argv[])
+int main(int argc, char* argv[])
 {
-	return 0;
+	BulletOpenGLApplication demo;
+	return glutmain(argc, argv, 1024, 768, "Introduction to Game Physics with Bullet Physics and OpenGL", &demo);
 }
 
